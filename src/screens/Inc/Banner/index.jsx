f4245b9b8e18a994380cf2c8719df420d1c07114
GuriWeb31 <guribwi123@gmail.com>
2025-11-25 15:05:25 +0530

    banner create

--- src/screens/Inc/Banner/index.jsx
+++ src/screens/Inc/Banner/index.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import close from '../../../assets/images/close.png'
 
 const Banner = () => {
   return (
-    <div className='text-center'>Hello</div>
+    <section className='py-15 px-5'>
+      <div className='max-w-screen-xl m-auto'>
+        <div className='grid grid-cols-1 xl:grid-cols-3 md:grid-cols-3 lg:grid-cols-3'>
+          <div className=''>
+            <h1 className='text-[48px]'>Indulge in Pure </h1>
+          </div>
+          <div className=''>
+            <img src={close} alt='' className='w-full rounded-t-[300px]'/>
+          </div>
+          <div className=''>
+            <div className=''>fhkjdshdsjkfhkds</div>
+          </div>
+        </div>
+
+      </div>
+      </section>
   )
 }
 
--- src/screens/Inc/Navbar.jsx
+++ src/screens/Inc/Navbar.jsx
@@ -1,29 +1,35 @@
 import React from 'react'
+import cart from '../../assets/images/cart.svg'
+import heart from '../../assets/images/heart.svg'
 
 const Navbar = () => {
   return (
    
 
-<nav class="">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 grid grid-cols-3">
-  <a href="" class="flex items-center space-x-3 rtl:space-x-reverse order-2 justify-center text-[32px] font-semibold">GLOWVY</a>
-  <div class="inline-flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+<nav class="px-5">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto grid grid-cols-3">
+  <a href="" class="flex items-center space-x-3 rtl:space-x-reverse order-2 xl:order-2 md:order-2 lg:order-2 sm:order-1 justify-center text-[32px] font-semibold">GLOWVY</a>
+  <div class="inline-flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse w-fit ml-auto">
+    
+        <img src={heart} alt='heart' className=' w-[32px] mr-6'/>
+        <img src={cart} alt='cart' className=' w-[32px] mr-10'/>
+    
       <button type="button" class="text-white bg-[#011E10] px-8 py-3 rounded-[10px] ml-auto">Sign Up</button>
       <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-9 h-9 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary" aria-controls="navbar-cta" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
         <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/></svg>
     </button>
   </div>
-  <div class="items-center justify-start hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-    <ul class="flex space-x-4.5">
+  <div class="items-center justify-start hidden w-full md:flex md:w-auto md:order-1 py-5" id="navbar-cta">
+    <ul class="flex space-x-10">
       <li>
-        <a href="#" class="block py-2 px-3" aria-current="page">Category</a>
+        <a href="#" class="block py-2 text-[#222222] hover:text-[#000] duration-300" aria-current="page">Category</a>
       </li>
       <li>
-        <a href="#" class="block py-2 px-3">Lifestyle</a>
+        <a href="#" class="block py-2 text-[#222222] hover:text-[#000] duration-300">Lifestyle</a>
       </li>
       <li>
-        <a href="#" class="block py-2 px-3">Brands</a>
+        <a href="#" class="block py-2 text-[#222222] hover:text-[#000] duration-300">Brands</a>
       </li>
       
     </ul>
